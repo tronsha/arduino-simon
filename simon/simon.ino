80c6ad74049907f0a1561861a8bd32341b4e278f
Stefan Hüsges <tronsha@gmx.de>
2015-10-06 16:26:13 +0200

    update

--- simon/simon.ino
+++ simon/simon.ino
@@ -11,6 +11,7 @@ float sound[] = {659.255, 554.365, 440.000, 329.628};
 
 int count = 0;
 int values[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int speedLevel = 1200;
 
 void fnBlue(int t) {
   digitalWrite(pinLed[blue], HIGH);
@@ -53,6 +54,95 @@ void reset() {
   fnYellow(1000);
   fnRed(1000);
   fnGreen(1000);
+  delay(2000);
+}
+
+void lose() {
+  speedLevel = speedLevel + 200;
+  reset();
+}
+
+void win() {
+  speedLevel = speedLevel - 200;
+  reset();
+}
+
+void showLeds() {
+  for (int i = 0; i < count; i++) {
+    if (values[i] == 0) {
+      fnBlue(speedLevel);
+    }
+    if (values[i] == 1) {
+      fnYellow(speedLevel);
+    }
+    if (values[i] == 2) {
+      fnRed(speedLevel);
+    }
+    if (values[i] == 3) {
+      fnGreen(speedLevel);
+    }
+  }
+}
+
+void readButtons() {
+  int pos = 0;
+  while (true) {
+    if (values[pos] == blue) {
+      if (digitalRead(pinButton[yellow]) == LOW || digitalRead(pinButton[red]) == LOW || digitalRead(pinButton[green]) == LOW) {
+        lose();
+      }
+      if (digitalRead(pinButton[blue]) == LOW) {
+        while (true) {
+          if (digitalRead(pinButton[blue]) == HIGH) {
+            break;
+          }
+        }
+        pos++;
+      }
+    }
+    if (values[pos] == yellow) {
+      if (digitalRead(pinButton[blue]) == LOW || digitalRead(pinButton[red]) == LOW || digitalRead(pinButton[green]) == LOW) {
+        lose();
+      }
+      if (digitalRead(pinButton[yellow]) == LOW) {
+        while (true) {
+          if (digitalRead(pinButton[yellow]) == HIGH) {
+            break;
+          }
+        }
+        pos++;
+      }
+    }
+    if (values[pos] == red) {
+      if (digitalRead(pinButton[blue]) == LOW || digitalRead(pinButton[yellow]) == LOW || digitalRead(pinButton[green]) == LOW) {
+        lose();
+      }
+      if (digitalRead(pinButton[red]) == LOW) {
+        while (true) {
+          if (digitalRead(pinButton[red]) == HIGH) {
+            break;
+          }
+        }
+        pos++;
+      }
+    }
+    if (values[pos] == green) {
+      if (digitalRead(pinButton[blue]) == LOW || digitalRead(pinButton[yellow]) == LOW || digitalRead(pinButton[red]) == LOW) {
+        lose();
+      }
+      if (digitalRead(pinButton[green]) == LOW) {
+        while (true) {
+          if (digitalRead(pinButton[green]) == HIGH) {
+            break;
+          }
+        }
+        pos++;
+      }
+    }
+    if (pos == count) {
+      return;
+    }
+  }
 }
 
 void setup() {
@@ -69,7 +159,15 @@ void setup() {
 
 void loop() {
   if (digitalRead(pinButton[blue]) == LOW && digitalRead(pinButton[yellow]) == LOW && digitalRead(pinButton[red]) == LOW && digitalRead(pinButton[green]) == LOW) {
+    speedLevel = 1200;
     reset();
+  }
+  values[count] = random(0, 3);
+  count++;
+  showLeds();
+  readButtons();
+  if (count == 20) {
+    win();
   }
   delay(500);
 }
