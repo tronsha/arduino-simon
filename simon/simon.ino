ce554fe94a963b674b72678fca22e19328912905
Stefan Hüsges <tronsha@gmx.de>
2017-06-04 19:52:03 +0200

    update

--- simon/simon.ino
+++ simon/simon.ino
@@ -1,111 +1,66 @@
-const int blue = 0;
-const int yellow = 1;
-const int red = 2;
-const int green = 3;
+struct light {
+  byte id;
+  byte led;
+  byte button;
+  float frequency;
+};
+
+const light blue = {0, 9, 5, 659.255};
+const light yellow = {1, 10, 6, 554.365};
+const light red = {2, 11, 7, 440.000};
+const light green = {3, 12, 8, 329.628};
+
+const light lights[] = {blue, yellow, red, green};
 
 const int pinSpeaker = 4;
-const int pinButton[] = {5, 6, 7, 8};
-const int pinLed[] = {9, 10, 11, 12};
-
-const float sound[] = {659.255, 554.365, 440.000, 329.628};
 
 int count = 0;
-int values[] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+byte values[100];
+
 int speedLevel = 1200;
 
-void fnBlue(int t) {
-  digitalWrite(pinLed[blue], HIGH);
-  tone(pinSpeaker, sound[blue], t);
-  delay(t);
+void outputLight(byte id) {
+  digitalWrite(lights[id].led, HIGH);
+  tone(pinSpeaker, lights[id].frequency, speedLevel);
+  delay(speedLevel);
   noTone(pinSpeaker);
-  digitalWrite(pinLed[blue], LOW);
-}
-
-void fnYellow(int t) {
-  digitalWrite(pinLed[yellow], HIGH);
-  tone(pinSpeaker, sound[yellow], t);
-  delay(t);
-  noTone(pinSpeaker);
-  digitalWrite(pinLed[yellow], LOW);
-}
-
-void fnRed(int t) {
-  digitalWrite(pinLed[red], HIGH);
-  tone(pinSpeaker, sound[red], t);
-  delay(t);
-  noTone(pinSpeaker);
-  digitalWrite(pinLed[red], LOW);
-}
-
-void fnGreen(int t) {
-  digitalWrite(pinLed[green], HIGH);
-  tone(pinSpeaker, sound[green], t);
-  delay(t);
-  noTone(pinSpeaker);
-  digitalWrite(pinLed[green], LOW);
+  digitalWrite(lights[id].led, LOW);
 }
 
 void allLedOn () {
-  digitalWrite(pinLed[blue], HIGH);
-  digitalWrite(pinLed[yellow], HIGH);
-  digitalWrite(pinLed[red], HIGH);
-  digitalWrite(pinLed[green], HIGH);
+  digitalWrite(lights[0].led, HIGH);
+  digitalWrite(lights[1].led, HIGH);
+  digitalWrite(lights[2].led, HIGH);
+  digitalWrite(lights[3].led, HIGH);
 }
 
 void allLedOff () {
-  digitalWrite(pinLed[blue], LOW);
-  digitalWrite(pinLed[yellow], LOW);
-  digitalWrite(pinLed[red], LOW);
-  digitalWrite(pinLed[green], LOW);
+  digitalWrite(lights[0].led, LOW);
+  digitalWrite(lights[1].led, LOW);
+  digitalWrite(lights[2].led, LOW);
+  digitalWrite(lights[3].led, LOW);
 }
 
 void reset() {
   count = 0;
-  for (int i = 0; i < 20; i++) {
+  for (int i = 0; i < 100; i++) {
     values[i] = -1;
-    allLedOn();
-    delay(50);
-    allLedOff();
-    delay(50);
   }
-  delay(2000);
 }
 
 void lose() {
-  allLedOn();
-  fnBlue(1000);
-  fnYellow(1000);
-  fnRed(1000);
-  fnGreen(1000);
   Serial.println("Oh no...");
-  if (speedLevel <= 1000) {
-    speedLevel = speedLevel + 200;
-  }
   reset();
 }
 
 void win() {
   Serial.println("Very good my master!");
-  if (speedLevel >= 400) {
-    speedLevel = speedLevel - 200;
-  }
   reset();
 }
 
 void showLeds() {
   for (int i = 0; i < count; i++) {
-    if (values[i] == 0) {
-      fnBlue(speedLevel);
-    }
-    if (values[i] == 1) {
-      fnYellow(speedLevel);
-    }
-    if (values[i] == 2) {
-      fnRed(speedLevel);
-    }
-    if (values[i] == 3) {
-      fnGreen(speedLevel);
-    }
+    outputLight(values[i]);
     delay(200);
   }
 }
@@ -114,64 +69,60 @@ void readButtons() {
   int pos = 0;
   while (true) {
     delay(50);
-    if (values[pos] == blue) {
-      //Serial.println("blue");
-      if (digitalRead(pinButton[yellow]) == LOW || digitalRead(pinButton[red]) == LOW || digitalRead(pinButton[green]) == LOW) {
+    if (values[pos] == blue.id) {
+      if (digitalRead(yellow.button) == LOW || digitalRead(red.button) == LOW || digitalRead(green.button) == LOW) {
         lose();
       }
-      if (digitalRead(pinButton[blue]) == LOW) {
+      if (digitalRead(blue.button) == LOW) {
         while (true) {
-          digitalWrite(pinLed[blue], HIGH);
-          if (digitalRead(pinButton[blue]) == HIGH) {
-            digitalWrite(pinLed[blue], LOW);
+          digitalWrite(blue.led, HIGH);
+          if (digitalRead(blue.button) == HIGH) {
+            digitalWrite(blue.led, LOW);
             break;
           }
         }
         pos++;
         continue;
       }
-    } else if (values[pos] == yellow) {
-      //Serial.println("yellow");
-      if (digitalRead(pinButton[blue]) == LOW || digitalRead(pinButton[red]) == LOW || digitalRead(pinButton[green]) == LOW) {
+    } else if (values[pos] == yellow.id) {
+      if (digitalRead(blue.button) == LOW || digitalRead(red.button) == LOW || digitalRead(green.button) == LOW) {
         lose();
       }
-      if (digitalRead(pinButton[yellow]) == LOW) {
+      if (digitalRead(yellow.button) == LOW) {
         while (true) {
-          digitalWrite(pinLed[yellow], HIGH);
-          if (digitalRead(pinButton[yellow]) == HIGH) {
-            digitalWrite(pinLed[yellow], LOW);
+          digitalWrite(yellow.led, HIGH);
+          if (digitalRead(yellow.button) == HIGH) {
+            digitalWrite(yellow.led, LOW);
             break;
           }
         }
         pos++;
         continue;
       }
-    } else if (values[pos] == red) {
-      //Serial.println("red");
-      if (digitalRead(pinButton[blue]) == LOW || digitalRead(pinButton[yellow]) == LOW || digitalRead(pinButton[green]) == LOW) {
+    } else if (values[pos] == red.id) {
+      if (digitalRead(blue.button) == LOW || digitalRead(yellow.button) == LOW || digitalRead(green.button) == LOW) {
         lose();
       }
-      if (digitalRead(pinButton[red]) == LOW) {
+      if (digitalRead(red.button) == LOW) {
         while (true) {
-          digitalWrite(pinLed[red], HIGH);
-          if (digitalRead(pinButton[red]) == HIGH) {
-            digitalWrite(pinLed[red], LOW);
+          digitalWrite(red.led, HIGH);
+          if (digitalRead(red.button) == HIGH) {
+            digitalWrite(red.led, LOW);
             break;
           }
         }
         pos++;
         continue;
       }
-    } else if (values[pos] == green) {
-      //Serial.println("green");
-      if (digitalRead(pinButton[blue]) == LOW || digitalRead(pinButton[yellow]) == LOW || digitalRead(pinButton[red]) == LOW) {
+    } else if (values[pos] == green.id) {
+      if (digitalRead(blue.button) == LOW || digitalRead(yellow.button) == LOW || digitalRead(red.button) == LOW) {
         lose();
       }
-      if (digitalRead(pinButton[green]) == LOW) {
+      if (digitalRead(green.button) == LOW) {
         while (true) {
-          digitalWrite(pinLed[green], HIGH);
-          if (digitalRead(pinButton[green]) == HIGH) {
-            digitalWrite(pinLed[green], LOW);
+          digitalWrite(green.led, HIGH);
+          if (digitalRead(green.button) == HIGH) {
+            digitalWrite(green.led, LOW);
             break;
           }
         }
@@ -188,14 +139,14 @@ void readButtons() {
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(0));
-  pinMode(pinButton[blue], INPUT_PULLUP);
-  pinMode(pinButton[yellow], INPUT_PULLUP);
-  pinMode(pinButton[red], INPUT_PULLUP);
-  pinMode(pinButton[green], INPUT_PULLUP);
-  pinMode(pinLed[blue], OUTPUT);
-  pinMode(pinLed[yellow], OUTPUT);
-  pinMode(pinLed[red], OUTPUT);
-  pinMode(pinLed[green], OUTPUT);
+  pinMode(lights[0].button, INPUT_PULLUP);
+  pinMode(lights[1].button, INPUT_PULLUP);
+  pinMode(lights[2].button, INPUT_PULLUP);
+  pinMode(lights[3].button, INPUT_PULLUP);
+  pinMode(lights[0].led, OUTPUT);
+  pinMode(lights[1].led, OUTPUT);
+  pinMode(lights[2].led, OUTPUT);
+  pinMode(lights[3].led, OUTPUT);
   reset();
 }
 
@@ -204,7 +155,7 @@ void loop() {
   count++;
   showLeds();
   readButtons();
-  if (count == 10) {
+  if (count == 100) {
     win();
   }
   delay(500);
